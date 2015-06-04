52ef4951e9f1693a7fc1c315404f80cabaec3513
Miguel Ángel Santamaría Rogado <ma.santamaria.rogado@gmail.com>
2015-06-04 14:11:03 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -33,17 +33,23 @@ var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz; // exportar la definición de la tabla Quiz
 
 // sequelize.sync() crea e inicializa la tabla de preguntas en la BBDD
-sequelize.sync().success(function () {
-  // success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function (count) {
+sequelize.sync().then(function () {
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count) {
     if (count === 0) { // la tabla se inicializa solo si está vacía
       Quiz.create(
         {
           pregunta: 'Capital de Italia',
           respuesta: 'Roma'
         }
+      );
+      Quiz.create(
+        {
+          pregunta: 'Capital de Portugal',
+          respuesta: 'Lisboa'
+        }
       )
-      .success(function () { console.log('Base de datos inicializada'); });
+      .then(function () { console.log('Base de datos inicializada'); });
     }
   });
 });
